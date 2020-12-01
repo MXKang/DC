a870680c081df71f54b9ab03513f20aaa336fdef
MXKang <atomr.xu@outlook.com>
2020-12-01 14:50:01 +0800

    测试自动化构建，第一次修改。

---
+++ Center/src/main/java/com/atom/AtomApp.java
@@ -0,0 +1,14 @@
+package com.atom;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtomApp.class, args);
+    }
+
+}
---
+++ Center/src/main/java/com/atom/controller/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.atom.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping(value = "/hello")
+    public String getHello(String name){
+        return "自动部署的网站";
+    }
+
+}
