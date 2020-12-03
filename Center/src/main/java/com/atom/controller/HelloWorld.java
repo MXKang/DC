4add0c7b26c547efd1c9cf3cf6d7e1baa727aa41
MXKang <atomr.xu@outlook.com>
2020-12-03 09:23:41 +0800

    测试自动化构建，第四次修改，修改展示内容。

--- Center/src/main/java/com/atom/controller/HelloWorld.java
+++ Center/src/main/java/com/atom/controller/HelloWorld.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorld {
     @GetMapping(value = "/hello")
     public String getHello(String name){
-        return "自动部署的网站";
+        return "自动部署的网站，二次进化";
     }
 
 }
