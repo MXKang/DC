83781ca5de1de0c5e11f04372076c2b4d1018e6a
MXKang <atomr.xu@outlook.com>
2021-01-29 10:55:48 +0800

    测试自动化构建，第七次修改，修改展示内容。

--- Center/src/main/java/com/atom/AtomApp.java
+++ Center/src/main/java/com/atom/AtomApp.java
@@ -1,10 +1,12 @@
 package com.atom;
 
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@NacosPropertySource(dataId = "Center",autoRefreshed = true)
 public class AtomApp {
 
     public static void main(String[] args) {
--- Center/src/main/java/com/atom/controller/HelloWorld.java
+++ Center/src/main/java/com/atom/controller/HelloWorld.java
@@ -1,13 +1,39 @@
 package com.atom.controller;
 
+import com.alibaba.nacos.api.annotation.NacosInjected;
+//import com.alibaba.nacos.api.config.annotation.NacosValue;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.NamingService;
+import com.alibaba.nacos.api.naming.pojo.Instance;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloWorld {
+
+//    @NacosValue(value = "${useLocalCache:AMD YES!}", autoRefreshed = true)
+//    private String useLocalCache;
+
+    @NacosInjected
+    private NamingService namingService;
+
     @GetMapping(value = "/hello")
-    public String getHello(String name){
-        return "自动部署的网站，二次进化！！！！！！！！！！！！！！！";
+    public String hello(String name){
+        return "自动部署的网站，二次进化金佛喷气机我就可怜见;";
     }
 
+//    @GetMapping(value = "/get")
+//    public String get(){
+//        return  useLocalCache;
+//    }
+
+    @GetMapping(value = "/value")
+    @ResponseBody
+    public List<Instance> value(@RequestParam String serviceName) throws NacosException {
+        return namingService.getAllInstances(serviceName);
+    }
 }
